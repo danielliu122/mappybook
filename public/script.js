13e69eef6fbee0082882d3a2021fb3a64130f333
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-06-02 21:56:01 -0400

    version 1

---
+++ public/script.js
@@ -0,0 +1,283 @@
+// require('/server.js');
+// console.log(postlocation);
+
+//get location url call
+var locations=[];
+
+function run() {
+  
+  // Creating Our XMLHttpRequest object 
+  var xhr = new XMLHttpRequest();
+
+  // Making our connection  
+  var url = '/locations.txt';
+  xhr.open("GET", url, true);
+
+  // function execute after request is successful 
+  xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+          locations=this.responseText;
+          locations=JSON.parse(locations);
+          console.log(locations);
+      }
+  }
+  // Sending our request 
+  xhr.send();
+}
+run();
+//get location url call
+
+function postLocation(location){
+  console.log("location in client"+ location);
+  var http = new XMLHttpRequest();
+  var url = '/test';
+  var params = location;
+  http.open('POST', url, true);
+
+  //Send the proper header information along with the request
+  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+  }
+  http.send(params);
+}
+//------------------------------------------------------------------------
+
+// In the following example, markers appear when the user clicks on the map.
+// Each marker is labeled with a single alphabetical character.
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = 0;
+
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function initAutocomplete() {
+  console.log("initializing mapscript...");
+  //locations= getLocations();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.468766197642246,  lng: -74.44103887469822, },
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+  
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  
+  
+  
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+  
+ // Adds a marker to the map and saves user data.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({ 
+    draggable: true, 
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map,
+  });
+     marker.addListener('rightclick', function() {
+    console.log("marker clicked");
+    //marker.setMap(null);
+    marker.setVisible(false);
+  });
+  var usrInput = prompt("Write something about this place. (if you want)", "");
+  if (usrInput == null){
+     marker.setMap(null);
+  }
+  else if (usrInput.length>7){ //relevant post
+    // save location to database
+    console.log("saving location..."+ location);
+    let temp2= ",,,";
+    let temp = location.toString().split(",");
+    console.log(temp);
+    let l1= temp[0].slice(1);
+    console.log("l1"+ l1);
+    let l2= temp[1].slice(0, temp[1].length - 1);
+    var l3  = { "lat": l1,  "lng": l2,  "content": usrInput};
+    l3=JSON.stringify(l3);
+    console.log(l3 + " "+ typeof l3);
+    
+
+    postLocation(l3);    
+
+  }
+  else{
+      marker.setMap(null);
+      infowindow.close();
+  }
+     var infowindow = new google.maps.InfoWindow({
+            content:usrInput
+          });
+  addMarkerListener(marker, infowindow);
+  
+  
+}
+  
+// longCLick function (to add marker)
+function LongClick(map, length) {
+  this.length_ = length;
+  var me = this;
+  me.map_ = map;
+  me.timeoutId_ = null;
+  google.maps.event.addListener(map, 'mousedown', function(e) {
+    me.onMouseDown_(e);
+  });
+  google.maps.event.addListener(map, 'mouseup', function(e) {
+    me.onMouseUp_(e);
+  });
+  google.maps.event.addListener(map, 'drag', function(e) {
+    me.onMapDrag_(e);
+  });
+};
+LongClick.prototype.onMouseUp_ = function(e) {
+  clearTimeout(this.timeoutId_);
+};
+LongClick.prototype.onMouseDown_ = function(e) {
+  clearTimeout(this.timeoutId_);
+  var map = this.map_;
+  var event = e;
+  this.timeoutId_ = setTimeout(function() {
+    google.maps.event.trigger(map, 'LongClick', event);
+  }, this.length_);
+};
+LongClick.prototype.onMapDrag_ = function(e) {
+  clearTimeout(this.timeoutId_);
+};
+  
+new LongClick(map, 400);
+google.maps.event.addListener(map, 'LongClick', function(event) {
+  // add marker on long click
+    addMarker(event.latLng, map);
+});
+  
+ // Array that stores the locations and the content about each location
+        // var locations = [
+        //   {lat: 40.468766197642246,  lng: -74.44103887469822,  content: "<p>First.</p>"},
+        // ]
+
+        //Use the first location as the center
+
+        // var mapOptions = {
+        //   center: new google.maps.LatLng(locations[0]['lat'],locations[0]['lng']),
+        //   zoom: 5,
+        //   zoomControl: true,  
+        // }
+
+        // var mapElement = document.getElementById('map');
+
+        // add location markers
+        console.log(locations);
+        for (i = 0; i < locations.length; i++) {
+          console.log(locations[i]);
+          console.log("Adding marker at lat="+ locations[i]["lat"] + ", long=" + locations[i]['lng']);
+
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i]["lat"], locations[i]['lng']),
+            map: map,
+          });
+
+          var infowindow = new google.maps.InfoWindow({
+            content: locations[i]['content']
+          });
+
+          // Now we are inside the closure or scope of this for loop,
+          // but we're calling a function that was defined in the global scope.
+          addMarkerListener(marker, infowindow);
+        }
+  
+  
+function addMarkerListener(marker, infowindow) {
+
+        marker.addListener('mouseover', function(e) {
+          infowindow.open(map,marker);
+        });
+
+        marker.addListener('mouseout', function() {        
+          infowindow.close();
+        });
+  // hide marker
+   marker.addListener('rightclick', function() {
+    console.log("marker clicked");
+    //marker.setMap(null);
+    
+    marker.setVisible(false);
+    infowindow.close();
+  });
+      } 
+  
+  
+  
+// end
+}
+//initAutocomplete();
+window.initAutocomplete = initAutocomplete;
---
+++ server.js
@@ -0,0 +1,136 @@
+require("dotenv").config();
+var bodyParser = require('body-parser')
+
+
+const fs = require('fs');
+
+
+var express = require("express");
+const path = require('path');
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use("/", express.static(__dirname + '/public'));
+app.get('/locations', function(req, res){
+  res.send(locations);
+});
+app.post('/test', function(req, res, next) {
+  // req.body contains the parsed xml 
+  //console.log(res.body);
+  //cannot use location as microsoft var
+  console.log(req.body);
+  var location2=JSON.stringify(req.body).toString();
+  console.log(location2);
+
+  let toReplace= '\'';
+  let l0=location2.replaceAll(/\\/g, '');
+
+  l0=l0.substring(2,l0.length-5);
+  console.log("lo "+l0);
+
+  let lat=l0.substring(l0.indexOf("lat")+6,l0.indexOf("lng")-3);
+  let lng=l0.substring(l0.indexOf("lng")+7,l0.indexOf("content")-3);;
+  let content= l0.substring(l0.indexOf("content")+10,l0.length-2);
+
+
+
+  //console.log("location in FINAL"+location2 +typeof location2);
+  postLocation(lat,lng,content);
+
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on localhost3000");
+});
+
+
+
+const uri = process.env['DB_URL'];
+
+
+const { MongoClient, FindCursor, Db } = require("mongodb");
+const { monitorEventLoopDelay } = require("perf_hooks");
+const { json } = require("express/lib/response");
+const client = new MongoClient(uri);
+var locations = [];
+
+function convert(obj) {
+  return Object.keys(obj).map(key => ({
+      name: key,
+      value: obj[key],
+      type: "foo"
+  }));
+}
+
+
+
+async function getLocations() {
+  try {
+    await client.connect();
+    const database = client.db('MapAppDatabase');
+    // return location data
+    var result= await database.collection('Responses').find().toArray();
+    //result = Object.entries(result);
+    //result.forEach(function(item){ delete item.id });  
+
+    locations=result;
+    //console.log(result );
+    //console.log(result.length);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      //await client.close();
+  }
+  console.log(locations);
+  return locations;
+}
+
+
+//getLocations();
+
+;(async () => {
+  locations = await getLocations();
+  //console.log(locations);
+  //console.log("server locations "+locations);
+  //let stringLocations= locations.toString();
+
+// write to a new file named 
+fs.writeFile('./public/locations.txt',  JSON.stringify(locations), (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('locations url created!');
+});
+})()
+
+
+async function postLocation(lat,lng,content) {
+  try {
+    await client.connect();
+    console.log("location in server"+lat+lng+content);
+
+    const database = client.db('MapAppDatabase');
+    const Responses = database.collection('Responses');
+    
+    
+    //Query 
+    //var doc = { "lat": 1,  "lng": 2,  "content": 3};
+    var toPost =  Responses.insertOne({
+      lat: lat,
+      lng: lng,
+      content: content
+    });
+    Responses.insertOne(toPost);
+    //clear database (not working)
+    //Responses.drop();
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // client.close();
+  }
+}
+//postLocation().catch(console.dir);
