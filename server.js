219401098ca8ddedf020bd14cfc12e3963d83166
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-06-02 22:39:35 -0400

    server test

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const express = require("express");
 const path = require('path');
 var app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
