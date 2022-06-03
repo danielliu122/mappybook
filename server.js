a6906efbe37173d2e87571d969ec02dd14494042
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-06-02 22:03:28 -0400

    test path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use("/", express.static(__dirname + '/public'));
+app.use("/", express.static(path.join (__dirname, 'public')));
 app.get('/locations', function(req, res){
   res.send(locations);
 });
