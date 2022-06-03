29566460eb9b7cd4d1a40dea13860672c2e6549e
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-06-03 15:04:10 -0400

    fixed wrong txt file

--- public/server.js
+++ public/server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json())
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", express.static(__dirname));
 
+
 // app.get('/', function(req, res){
 //   res.send('index.html');
 // });
