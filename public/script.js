e28ba0771109326954cc6938703cfeb9d2ff775a
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2023-09-24 23:55:44 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -29,20 +29,10 @@ function run() {
 
 function postLocation(location){
   console.log("location in client"+ location);
-  var http = new XMLHttpRequest();
-  var url = '/';
-  var params = location;
-  http.open('POST', url, true);
-
-  //Send the proper header information along with the request
-  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-  http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-  }
-  http.send(params);
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("POST", "/");
+  req.send(params);
 }
 //------------------------------------------------------------------------
 
