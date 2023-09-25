f7ff0ab53c528a11557f69e9ffbe461e513b242b
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2023-09-24 20:11:48 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -64,7 +64,10 @@ function initAutocomplete() {
     mapTypeId: "roadmap",
   });
   run();
-
+  setTimeout(() => {
+    console.log("Delayed for 5 seconds.");
+  }, "5000");
+  
 
   console.log(locations);
   addlocations(locations,map);
